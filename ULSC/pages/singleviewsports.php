c523ff9673b0764b5ccba277539d22b602cb3cea
harsh1036 <harshvoracomp@gmail.com>
2025-03-24 15:23:36 +0530

    Updated into a view ulsc

--- ULSC/pages/singleviewsports.php
+++ ULSC/pages/singleviewsports.php
@@ -50,7 +50,7 @@ if (!empty($selected_event_id)) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Spoural Management System</title>
+    <title>Spoural Management Systems</title>
     <link rel="stylesheet" href="../assets/css/style.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     
